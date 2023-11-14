af3168e50b78f44fab3938203ff8b4801f9af4e9
vvvvtrt <sleim2000@gmail.com>
2023-11-14 21:25:19 +0300

    Добавьте файлы проекта.

---
+++ complex/complex.cpp
@@ -0,0 +1,257 @@
+﻿#include "complex.hpp"
+
+
+std::ostream& Complex::writeTo(std::ostream& ostrm) const
+{
+	ostrm << leftBrace << re << separator << im << rightBrace;
+	return ostrm;
+}
+
+
+std::istream& Complex::readFrom(std::istream& istrm)
+{
+	char leftBrace(0);
+	double real(0.0);
+	char comma(0);
+	double imaganary(0.0);
+	char rightBrace(0);
+
+	istrm >> leftBrace >> real >> comma >> imaganary >> rightBrace;
+
+	if (istrm.good()) {
+		if ((Complex::leftBrace == leftBrace) && (Complex::separator == comma)
+			&& (Complex::rightBrace == rightBrace)) {
+			re = real;
+			im = imaganary;
+		}
+		else {
+			istrm.setstate(std::ios_base::failbit);
+		}
+	}
+	return istrm;
+}
+
+void Complex::pow(int base) {
+	Complex term(re, im);
+
+	Complex ans = Complex(1);
+
+	if (base > 0) {
+		for (int i = 0; i < std::abs(base); i++) {
+			ans = ans * term;
+		}
+	}
+
+	if (base < 0) {
+		term = (1.0 / term);
+
+		for (int i = 0; i < std::abs(base); i++) {
+			ans = ans * term;
+		}
+	}
+
+	re = ans.re;
+	im = ans.im;
+
+}
+
+double Complex::argument() {
+	if (re > 0) {
+		return std::atan(im / re);
+	}
+	else if (re < 0 && im >= 0) {
+		return pi + std::atan(im / re);
+	}
+	else if (re < 0 && im < 0) {
+		return -pi + std::atan(im / re);
+	}
+	else if (re == 0 && im > 0) {
+		return pi / 2;
+	}
+	else {
+		return -(pi / 2);
+	}
+}
+
+
+void errors(std::string message) {
+	std::cerr << message << "\n" << std::endl;
+	std::exit(1);
+}
+
+double abs(const Complex& num) {
+	return (std::sqrt(num.re * num.re + num.im * num.im));
+}
+
+Complex pow(const Complex& lhs, int n)
+{
+	Complex c0 = { 0,0 };
+	if (lhs == c0 && n != 0) {
+		return { 0,0 };
+	}
+	else if (lhs == c0 && n == 0) {
+		errors("ERROR! Zero to the zero degree");
+	}
+	else if (n == 0) {
+		return Complex(1);
+	}
+	else if (n > 0) {
+		return lhs * pow(lhs, --n);
+	}
+	else if (n < 0) {
+		Complex olhs = (1.0 / lhs);
+		return olhs * pow(lhs, ++n);
+	}
+}
+
+
+
+double argument(const Complex& num) {
+	if (num.re > 0) {
+		return std::atan(num.im / num.re);
+	}
+	else if (num.re < 0 && num.im >= 0) {
+		return pi + std::atan(num.im / num.re);
+	}
+	else if (num.re < 0 && num.im < 0) {
+		return -pi + std::atan(num.im / num.re);
+	}
+	else if (num.re == 0 && num.im > 0) {
+		return pi / 2;
+	}
+	else {
+		return -(pi / 2);
+	}
+}
+
+Complex conjugate(const Complex& num) {
+	return Complex(num.re, -num.im);
+}
+
+
+void Complex::sqrt(int base) {
+	Complex a(re, im);
+
+	double rootArgument = a.argument() / base;
+	double a_abs = a.abs();
+	double rootModulus = std::pow(a_abs, 1.0 / base);
+
+	re = rootModulus * std::cos(rootArgument);
+	im = rootModulus * std::sin(rootArgument);
+}
+
+Complex sqrt(Complex num, int base) {
+	double rootArgument = num.argument() / base;
+	double a_abs = num.abs();
+	double rootModulus = std::pow(a_abs, 1.0 / base);
+
+	return Complex(rootModulus * std::cos(rootArgument), rootModulus * std::sin(rootArgument));
+}
+
+Complex::Complex(const double real)
+	: Complex(real, 0.0)
+{
+}
+
+Complex::Complex(const double real, const double imaginary)
+	: re(real)
+	, im(imaginary)
+{
+}
+
+Complex& Complex::operator+=(const Complex& rhs)
+{
+	re += rhs.re;
+	im += rhs.im;
+	return *this;
+}
+
+Complex operator+(const Complex& lhs, const Complex& rhs)
+{
+	return Complex(lhs.re + rhs.re, lhs.im + rhs.im);
+}
+
+Complex operator+(const double& lhs, const Complex& rhs) { 
+	return operator+(Complex(lhs), rhs); 
+}
+
+Complex operator+(const Complex& lhs, const double& rhs) { 
+	return operator+(lhs, Complex(rhs)); 
+}
+
+
+Complex& Complex::operator-=(const Complex& rhs)
+{
+	re -= rhs.re;
+	im -= rhs.im;
+	return *this;
+}
+
+Complex operator-(const Complex& lhs, const Complex& rhs)
+{
+	return Complex(lhs.re - rhs.re, lhs.im - rhs.im);
+}
+
+Complex operator-(const double& lhs, const Complex& rhs) {
+	return operator-(Complex(lhs), rhs);
+}
+
+Complex operator-(const Complex& lhs, const double& rhs) {
+	return operator-(lhs, Complex(rhs));
+}
+
+
+
+Complex& Complex::operator*=(const Complex& rhs)
+{
+	double re_new = re * rhs.re - im * rhs.im;
+
+	im = re * rhs.im + im * rhs.re;
+	re = re_new;
+
+	return *this;
+}
+
+Complex operator*(const Complex& lhs, const Complex& rhs)
+{
+	return Complex(lhs.re * rhs.re - lhs.im * rhs.im, lhs.re * rhs.im + lhs.im * rhs.re);
+}
+
+Complex operator*(const double& lhs, const Complex& rhs) { 
+	return operator*(Complex(lhs), rhs); 
+}
+
+Complex operator*(const Complex& lhs, const double& rhs) { 
+	return operator*(lhs, Complex(rhs)); 
+}
+
+
+Complex& Complex::operator/=(const Complex& rhs)
+{
+	if (rhs.re == 0 && rhs.im == 0) {
+		errors("Error! Division by zero!");
+	}
+
+	double temp_re = (re * rhs.re + im * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im);
+	im = (im * rhs.re - re * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im);
+	re = temp_re;
+	return *this;
+}
+
+Complex operator/(const Complex& lhs, const Complex& rhs)
+{
+	if (rhs.re == 0 && rhs.im == 0) {
+		errors("Error! Division by zero!");
+	}
+
+	return Complex(((lhs.re * rhs.re + lhs.im * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im)), ((lhs.im * rhs.re - lhs.re * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im)));
+}
+
+Complex operator/(const double& lhs, const Complex& rhs) {
+	return operator/(Complex(lhs), rhs); 
+}
+
+Complex operator/(const Complex& lhs, const double& rhs) {
+	return operator/(lhs, Complex(rhs)); 
+}
+
---
+++ complex/complex.hpp
@@ -0,0 +1,93 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+#define pi 3.14
+
+struct Complex {
+	Complex() {}
+	explicit Complex(const double real);
+	Complex(const double real, const double imaginary);
+
+
+	bool operator==(const Complex& rhs) const { return (re == rhs.re && im == rhs.im); }
+	bool operator!=(const Complex& rhs) const { return (!(operator==(rhs))); }
+
+	bool operator>(const Complex& rhs) const { return (std::sqrt(re * re + im * im) > std::sqrt(rhs.re * rhs.re + rhs.im * rhs.im)); }
+	bool operator<(const Complex& rhs) const { return (std::sqrt(re * re + im * im) < std::sqrt(rhs.re * rhs.re + rhs.im * rhs.im)); }
+
+	bool operator>=(const Complex& rhs) const { return (operator==(rhs) || operator>(rhs)); }
+	bool operator<=(const Complex& rhs) const { return (operator==(rhs) || operator<(rhs)); }
+
+	Complex& operator+=(const Complex& rhs);
+	Complex& operator+=(const double rhs) { return (operator+=(Complex(rhs))); }
+
+	Complex& operator-=(const Complex& rhs);
+	Complex& operator-=(const double rhs) { return (operator-=(Complex(rhs))); }
+
+	Complex& operator*=(const Complex& rhs);
+	Complex& operator*=(const double rhs) { return (operator*=(Complex(rhs))); }
+
+	Complex& operator/=(const Complex& rhs);
+	Complex& operator/=(const double rhs) { return (operator/=(Complex(rhs))); }
+
+	std::ostream& writeTo(std::ostream& ostrm) const;
+	std::istream& readFrom(std::istream& istrm);
+
+	double abs() { return std::sqrt(re * re + im * im); }
+	void conjugate() { im = -im; }
+
+	void pow(int base);
+	double argument();
+	void sqrt(int base);
+
+
+
+	double re{ 0.0 };
+	double im{ 0.0 };
+
+	static const char leftBrace{ '{' };
+	static const char separator{ ',' };
+	static const char rightBrace{ '}' };
+};
+
+
+
+Complex operator+(const Complex& lhs, const Complex& rhs);
+Complex operator+(const double& lhs, const Complex& rhs); 
+Complex operator+(const Complex& lhs, const double& rhs); 
+
+Complex operator-(const Complex& lhs, const Complex& rhs);
+Complex operator-(const double& lhs, const Complex& rhs); 
+Complex operator-(const Complex& lhs, const double& rhs); 
+
+Complex operator*(const Complex& lhs, const Complex& rhs);
+Complex operator*(const double& lhs, const Complex& rhs); 
+Complex operator*(const Complex& lhs, const double& rhs); 
+
+Complex operator/(const Complex& lhs, const Complex& rhs);
+Complex operator/(const double& lhs, const Complex& rhs); 
+Complex operator/(const Complex& lhs, const double& rhs); 
+
+
+double abs(const Complex& num);
+double argument(const Complex& num);
+
+Complex pow(const Complex& lhs, int n);
+Complex conjugate(const Complex& num);
+Complex sqrt(Complex num, int base);
+
+void errors(std::string message);
+
+
+inline std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs)
+{
+	return rhs.writeTo(ostrm);
+}
+
+inline std::istream& operator>>(std::istream& istrm, Complex& rhs)
+{
+	return rhs.readFrom(istrm);
+}
